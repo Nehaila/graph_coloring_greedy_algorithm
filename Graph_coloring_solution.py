cc9c11c0388e688549302353f9c09248d363e7f5
Nehaila <kassabinehaila@gmail.com>
2020-11-24 03:58:15 +0100

    commit

---
+++ Graph_coloring_solution.py
@@ -0,0 +1,63 @@
+enter n
+7
+7
+enter number of adjacents
+2
+enter adjacents to 0 from 1 to 7
+3
+enter adjacents to 0 from 1 to 7
+6
+enter number of adjacents
+2
+enter adjacents to 1 from 1 to 7
+2
+enter adjacents to 1 from 1 to 7
+3
+enter number of adjacents
+4
+enter adjacents to 2 from 1 to 7
+1
+enter adjacents to 2 from 1 to 7
+3
+enter adjacents to 2 from 1 to 7
+5
+enter adjacents to 2 from 1 to 7
+6
+enter number of adjacents
+4
+enter adjacents to 3 from 1 to 7
+0
+enter adjacents to 3 from 1 to 7
+1
+enter adjacents to 3 from 1 to 7
+2
+enter adjacents to 3 from 1 to 7
+5
+enter number of adjacents
+2
+enter adjacents to 4 from 1 to 7
+5
+enter adjacents to 4 from 1 to 7
+6
+enter number of adjacents
+3
+enter adjacents to 5 from 1 to 7
+3
+enter adjacents to 5 from 1 to 7
+4
+enter adjacents to 5 from 1 to 7
+2
+enter number of adjacents
+3
+enter adjacents to 6 from 1 to 7
+2
+enter adjacents to 6 from 1 to 7
+4
+enter adjacents to 6 from 1 to 7
+0
+{0: [3, 6], 1: [2, 3], 2: [1, 3, 5, 6], 3: [0, 1, 2, 5], 4: [5, 6], 5: [3, 4, 2], 6: [2, 4, 0]}
+{0: <Color #ffd300>, 1: <Color #ffd300>, 2: <Color #b20000>, 3: <Color DarkOrange>, 4: <Color #ffd300>, 5: <Color #ff4000>, 6: <Color DarkOrange>}
+[0, 1, 2, 3, 4, 5, 6]
+[(0, 3), (0, 6), (1, 2), (1, 3), (2, 3), (2, 5), (2, 6), (3, 5), (4, 5), (4, 6)]
+
+***Repl Closed***
---
+++ Graph_coloring_solution2.py
@@ -0,0 +1,35 @@
+enter n
+5
+5
+enter number of adjacents to 0
+1
+enter adjacents to 0 from 1 to 5
+3
+enter number of adjacents to 1
+1
+enter adjacents to 1 from 1 to 5
+3
+enter number of adjacents to 2
+1
+enter adjacents to 2 from 1 to 5
+3
+enter number of adjacents to 3
+4
+enter adjacents to 3 from 1 to 5
+0
+enter adjacents to 3 from 1 to 5
+1
+enter adjacents to 3 from 1 to 5
+2
+enter adjacents to 3 from 1 to 5
+4
+enter number of adjacents to 4
+1
+enter adjacents to 4 from 1 to 5
+3
+{0: [3], 1: [3], 2: [3], 3: [0, 1, 2, 4], 4: [3]}
+{0: <Color #ffd300>, 1: <Color #ffd300>, 2: <Color #ffd300>, 3: <Color DarkOrange>, 4: <Color #ffd300>}
+[0, 1, 2, 3, 4]
+[(0, 3), (1, 3), (2, 3), (3, 4)]
+
+***Repl Closed***
